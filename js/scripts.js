2adae0eefaedd70ef395de55f8a92d98cf9d0fbe
Cliff Nowicki <cliffn@eview360.com>
2016-10-17 15:29:47 -0400

    first files to commit

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function() {
+
+
+// ----- unslider example
+
+// $('.my-slider').unslider({
+//   autoplay: true
+// });
+
+
+$('.flexslider').flexslider({
+    animation: "slide",
+    slideshowSpeed: 7000
+  });
+
+
+});
